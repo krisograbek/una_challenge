37aa38b65be961143e90dc29491b6c8b6c7b1e97
krisograbek <krzysztof.ograbek@gmail.com>
2022-02-11 12:58:45 +0100

    design updates

--- frontend/src/components/FilterPanel.js
+++ frontend/src/components/FilterPanel.js
@@ -5,39 +5,40 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
 function FilterPanel(props) {
   const { handleFilter, user, handleChange, availableUsers } = props;
 
-  // const users = ['None', 'a', 'b', 'c', 'aaa', 'bbb', 'ccc'];
-
-  // React.useEffect(() => {
-  //   fetch(`/api/v1/level?user=${user}`).then(res => res.json()).then(data => {
-  //     setGlucoseData(data)
-  //   })
-  // }, [])
-
-
   return (
-    <Box >
-      <FormControl sx={{ minWidth: 120, mt: 2, mb: 2 }}>
-        <InputLabel id="demo-simple-select-label">User</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={user}
-          label="User"
-          onChange={handleChange}
-        >
-          {availableUsers.map((name, i) => {
-            return (
-              <MenuItem key={name} value={name}>{name}</MenuItem>
-            )
-          })}
-        </Select>
-        <Button variant="contained" onClick={() => handleFilter(user)}>Update</Button>
-      </FormControl>
-    </Box>
+    <Box sx={{ padding: 3, textAlign: "center" }}>
+      <Grid container direction="column" spacing={1}>
+        <Grid item>
+          <Typography variant="h6">Select User</Typography>
+          <FormControl sx={{ minWidth: 120, mt: 2 }}>
+            <InputLabel id="demo-simple-select-label">User</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={user}
+              label="User"
+              onChange={handleChange}
+            >
+              {availableUsers.map((name, i) => {
+                return (
+                  <MenuItem key={name} value={name}>{name}</MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item>
+          <Button variant="contained" onClick={() => handleFilter(user)}>Update</Button>
+        </Grid>
+      </Grid>
+    </Box >
   );
 }
 
